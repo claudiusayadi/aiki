428f50cd66f7c34fdddff2d559007c74d663170c
Claudius A <ayadiclaudius@gmail.com>
2025-10-06 19:25:10 +0100

    chore: set up CI/CD workflow and implement secrets management script

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+
+import { AuthModule } from './modules/auth/auth.module';
+import { EmailModule } from './modules/email/email.module';
+import { HealthModule } from './modules/health/health.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { PlansModule } from './modules/plans/plans.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { UsersModule } from './modules/users/users.module';
+
+import { validateEnv } from './core/config/app.config';
+import { apiProviders } from './core/config/providers.config';
+import throttlerConfig from './core/config/throttler.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      validate: validateEnv,
+    }),
+    ThrottlerModule.forRootAsync(throttlerConfig.asProvider()),
+    AuthModule,
+    EmailModule,
+    HealthModule,
+    PaymentsModule,
+    PlansModule,
+    TasksModule,
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [...apiProviders],
 })
 export class AppModule {}
