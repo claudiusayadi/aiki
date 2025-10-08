c16a7ca442af5be0fe3306f73c11ad08add93ee7
Claudius A <ayadiclaudius@gmail.com>
2025-10-08 17:26:49 +0100

    feat(dto): add PaginationDto and SortDto for handling pagination and sorting

---
+++ src/core/common/dto/query.dto.ts
@@ -0,0 +1,5 @@
+import { IntersectionType } from '@nestjs/swagger';
+import { PaginationDto } from './pagination.dto';
+import { SortDto } from './sort.dto';
+
+export class QueryDto extends IntersectionType(PaginationDto, SortDto) {}
